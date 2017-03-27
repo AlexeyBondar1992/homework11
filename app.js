f4c3dc7baca2e50be641729f4027a65835edfde9
AlexeyBondar1992 <metallhead92@mail.ru>
2017-03-27 20:53:10 +0300

    as i can

---
+++ app.js
@@ -0,0 +1,71 @@
+﻿function createCurrencyList(list) {
+    var select = document.createElement('select');
+    list.forEach(function (currency) {
+        var option = document.createElement('option');
+        option.value = currency.Cur_Abbreviation;
+        option.textContent = currency.Cur_Name;
+        select.appendChild(option);
+    });
+    return select;
+}
+
+function makeRequest(params, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(params.method, params.url, true);
+    xhr.responseType = 'json';
+    xhr.addEventListener('loadend', function listener() {
+        xhr.removeEventListener('loadend', listener);
+        callback(xhr.response);
+    });
+    xhr.send();
+}
+
+function loadCurrencyRate(code, callback) {
+    makeRequest({
+        method: 'GET',
+        url: 'http://www.nbrb.by/API/ExRates/Rates/' + code + '?ParamMode=2'
+    }, callback);
+}
+function loaderCurrencyRate(select, input, p) {
+    var currencyCode = select.value;
+    loadCurrencyRate(currencyCode, function (rate) {
+        resolver(input, rate, p);
+    });
+}
+
+
+makeRequest({
+    method: 'GET',
+    url: 'http://www.nbrb.by/API/ExRates/Currencies'
+}, function (list) {
+    var select = createCurrencyList(list),
+        input = createInput(),
+        p = document.createElement('p');
+    loaderCurrencyRate(select, input, p);
+    select.addEventListener('change', function () {
+        loaderCurrencyRate(select, input, p);
+    });
+    input.addEventListener('change', function () {
+        loaderCurrencyRate(select, input, p);
+    });
+    document.body.appendChild(select);
+    document.body.appendChild(input);
+    document.body.appendChild(p);
+
+});
+
+
+function createInput() {
+    var input = document.createElement('input');
+    input.type = 'number';
+    input.placeholder = 'введите сумму';
+    return input;
+}
+
+
+function resolver(input, object, p) {
+    var summ = (input.value * object.Cur_OfficialRate / object.Cur_Scale).toFixed(2);
+    p.textContent = `= ${summ} BYN`;
+}
+document.body.style.display = 'flex';
+
